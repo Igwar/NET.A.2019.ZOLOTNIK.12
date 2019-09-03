abecbe22780e06918436eca8563d8b19cc3fa201
Igwar <43217217+Igwar@users.noreply.github.com>
2019-09-03 16:41:05 +0300

    Add files via upload

---
+++ FibonacciLib/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciLib
+{
+    public static class FibNum
+    {
+        public static List<int> Find(int n)
+        {
+            List<int> result = new List<int>();
+            int n1 = 1;
+            int n2 = 1;
+            result.Add(n1);
+            result.Add(n2);
+            for (int i = 3; i <= n; ++i)
+            {
+                var n3 = n1 + n2;
+                result.Add(n3);
+                n1 = n2;
+                n2 = n3;
+
+            }
+
+            return result;
+        }
+    }
+}
---
+++ FibonacciLibTests/FibNumTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FibonacciLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciLib.Tests
+{
+    [TestClass()]
+    public class FibNumTests
+    {
+        [TestMethod()]
+        public void FindTest()
+        {
+            List<int> expect = new List<int> { 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987 };
+            List<int> result = new List<int>();
+            result = FibNum.Find(expect.Count);
+            for (int i = 0; i < expect.Count; i++)
+            {
+
+                Assert.AreEqual(expect[i],result[i] );
+            }
+        }
+    }
+}
